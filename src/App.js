564de08986d45a415841997a5241c31b87e64eee
Kaneez98Fatima <kaneez98fatima@gmail.com>
2021-10-18 23:40:26 +0500

    Upload files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TodoWebList from './components/TodoWebList';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoWebList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoWebList.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import TodoWebForm from './TodoWebForm';
+import Todo from './Todo';
+
+function TodoWebList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = todo => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
+  
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+
+
+  const removeTodo = id => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+  };
+
+  
+  return (
+    <>
+      <h1>What's the most useless talent you have?</h1>
+      <TodoWebForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
+}
+
+export default TodoWebList;
